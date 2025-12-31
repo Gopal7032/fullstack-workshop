67f341116a15e31fd402f9977d389f107bdc36c4
Gopal7032 <130591130+Gopal7032@users.noreply.github.com>
2025-12-31 09:33:24 +0530

    Delete 09-mysql/mysql1.sql

--- 09-mysql/mysql1.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE employees (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100),
-    department VARCHAR(50),
-    salary DECIMAL(10,2),
-    hire_date DATE,
-    manager_id INT
-);
